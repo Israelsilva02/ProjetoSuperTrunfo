3024f59fae2bd929781627515f9f9c2f2b6545dd
Israel Bernardo <101605470+Israelsilva02@users.noreply.github.com>
2024-04-04 23:17:04 -0300

    Update CartaComparacaoEstrategia.java

--- src/projetosupertrunfo/dto/CartaComparacaoEstrategia.java
+++ src/projetosupertrunfo/dto/CartaComparacaoEstrategia.java
@@ -3,7 +3,7 @@ package projetosupertrunfo.dto;
 import projetosupertrunfo.card.Carta;
 
 public class CartaComparacaoEstrategia {
-    // Método para comparar duas cartas e determinar qual é a vencedora
+ 
     public Carta compararCartas(Carta... cartas) {
         if (cartas.length == 0) {
             throw new IllegalArgumentException("Pelo menos uma carta deve ser fornecida.");
@@ -14,11 +14,11 @@ public class CartaComparacaoEstrategia {
         for (int i = 1; i < cartas.length; i++) {
             Carta cartaAtual = cartas[i];
 
-            // Comparação com base no ataque
+            
             if (cartaAtual.getAtaque() > vencedora.getAtaque()) {
                 vencedora = cartaAtual;
             } else if (cartaAtual.getAtaque() == vencedora.getAtaque()) {
-                // Se o ataque for igual, compare a defesa
+             
                 if (cartaAtual.getDefesa() > vencedora.getDefesa()) {
                     vencedora = cartaAtual;
                 }
